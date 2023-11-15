07653f72ab8c1ac558cf15c647a340ac299c551b
unknown <steven01.shi@hotmail.com>
2023-11-15 16:14:16 -0500

    Coded up friends.ts to include functionalities for adding friends

--- client/friends.ts
+++ client/friends.ts
@@ -7,37 +7,89 @@ export interface IFriend {
   invited: boolean;
 }
 
-let friends: IFriend[];
+let friends: IFriend[] = loadFriends();
+const addFriendButton = document.getElementById('friendListContainer');
+if (addFriendButton) addFriendButton.addEventListener('click', onAddFriend);
 
-function loadFriends(): IFriend[] {
-  // TODO: read friends from local storage
-  return [];
+function loadFriends(): IFriend[] { 
+  // read friends from local storage
+  const friendList = localStorage.getItem('friends');
+  if (friendList) {
+    friends = JSON.parse(friendList);
+  } else {
+    friends = []
+  }
+  return friends;
 }
 
-function saveFriends(): void {
-  // TODO: save friends to local storage
+function saveFriends(): void { 
+  // save friends to local storage
+  localStorage.setItem('friends', JSON.stringify(friends));
 }
 
 function createRawFriendElement(friend: IFriend): HTMLElement {
-  // TODO: create an HTML friend element without any listeners attached
-  return new HTMLElement();
+  // create an HTML friend element without any listeners attached
+  const newFriend = document.createElement('div');
+  newFriend.setAttribute('class', 'friend');
+  newFriend.innerHTML = `
+    <div class="friendDets">Name: ${friend.displayName}</div>
+    <div class="friendDets">Email:${friend.email}</div>
+  `;
+  return newFriend;
 }
 
 function addBehaviorToFriendElement(friendEmnt: HTMLElement): HTMLElement {
-  // TODO: add required listeners to the  HTML friend element
-  return new HTMLElement();
+  // add required listeners to the HTML friend element
+  return friendEmnt;
 }
 
 function appendFriendElementToDocument(friendEmnt: HTMLElement): void {
-  // TODO: add HTML friend element with listeners to the right HTML elememnt in the document
+  // add HTML friend element with listeners 
+  // to the right HTML elememnt in the document
+  const friendListContainer = document.getElementById('friendListContainer'); 
+  if (friendListContainer) {
+    friendListContainer.appendChild(friendEmnt);
+  }
 }
 
 function loadFriendsIntoDocument(): void {
-  // TODO: read friends from local storage and add them to the document
+  // read friends from local storage and add them to the document
+  loadFriends();
+  const friendListContainer = document.getElementById('friendListContainer');
+  if (friendListContainer) {
+    friendListContainer.innerHTML = '';
+    friends.forEach((friend) => {
+      const friendElmnt = createRawFriendElement(friend);
+      const friendWithBehavior = addBehaviorToFriendElement(friendElmnt);
+      appendFriendElementToDocument(friendWithBehavior);
+    });
+  }
 }
 
 function onAddFriend(): void {
-  // TODO: event handler to create a new friend from form info and append it to right HTML element in the document
+  // event handler to create a new friend from form info and 
+  // append it to right HTML element in the document
+  const displayNameInput = 
+        <HTMLInputElement>document.getElementById('nameInput');
+  const emailInput = 
+        <HTMLInputElement>document.getElementById('emailInput');
+  if (displayNameInput && emailInput) {
+    const displayName = displayNameInput.value;
+    const email = emailInput.value;
+    const newFriend: IFriend = {
+      id: uuidV4(),
+      displayName: displayName,
+      email: email,
+      invited: false,
+    };
+    friends.push(newFriend);
+    saveFriends();
+    loadFriendsIntoDocument();
+
+    // Reset the form after adding a friend
+    displayNameInput.value = '';
+    emailInput.value = '';
+  }
 }
 
 function onDeleteFriend(): void {
