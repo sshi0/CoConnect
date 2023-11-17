9f6a9df083fe166cc1325acfaa6ff7f6819baf1c
Steven Shi <95527459+sshi0@users.noreply.github.com>
2023-11-17 19:13:34 +0000

    Added clear friend button and confirmation test for delete friend and clear friend

---
+++ backup/pages copy/chat.dc0cafea.js
@@ -0,0 +1,7 @@
+// import {ServerToClientEvents, ClientToServerEvents} from '../common/socket.interface';
+// const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
+localStorage.getItem("token"),document.addEventListener("DOMContentLoaded",async function(e){// Document-ready event handler
+e.preventDefault();// anything to TODO here?
+});//# sourceMappingURL=chat.dc0cafea.js.map
+
+//# sourceMappingURL=chat.dc0cafea.js.map
